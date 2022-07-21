ae82434e171bba5e1297304189496cd416da4092
stephbaker23 <78457358+stephbaker23@users.noreply.github.com>
2022-07-21 11:45:38 +1000

    Starter code for garden monitor

---
+++ gardenMonitorStarterCode.ino
@@ -0,0 +1,21 @@
+
+void setup() {
+  // Establish serial communications
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  // WiFi connection - uncomment the following 10 lines when ready to connect!
+//  if (WiFi.status() != WL_CONNECTED) {
+//    Serial.print("Attempting to connect to WIFI_NETWORK_NAME: ");
+//    Serial.println(WIFI_NETWORK_NAME);
+//    WiFi.begin(WIFI_NETWORK_NAME, WIFI_PASSWORD);
+//    while(WiFi.status() != WL_CONNECTED) {
+//      Serial.print("..");
+//      delay(5000);
+//    }
+//    Serial.println("\nConnected.");
+//  }
+
+}
